9922f049c45086e68b9ad595e0c7141f4194fe83
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-08-22 15:34:39 -0400

    Fixt style

--- src/components/Invitations/UserInvitations.jsx
+++ src/components/Invitations/UserInvitations.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
-import { useDispatch, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 import ActivityCard from '../Cards/ActivityCard';
-import Banner from '../Banner';
-import Avatar from '../Avatar';
 
 export default function UserInvitations() {
-    const token = useSelector((state) => state.login.token);
   const user = useSelector((state) => state.login.user);
   const {activity_invitation} =user
   const invitations = [...activity_invitation].reverse()
 
-  console.log("the activities",activity_invitation)
-    return (
-   
+    return (   
       invitations.map(item => <>
-        
             <ActivityCard {...item.detail} join_activity={item.join_activity} date={item.date} user_invited ={item.user} status={item.status} invitaion_id={item.invitaion_id} m={"my-20"}/>
-          
             </>)
-
-       
     )
 }
